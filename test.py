2a7e823f31276cde504145bc3f097019db7a1aca
Jairo Sanchez <jairosanchez@Jairos-MacBook-Air.local>
2020-12-04 13:18:58 -0800

    added a couple more templates and our first route

--- test.py
+++ test.py
@@ -1,9 +1,24 @@
 import jinja2
-from flask import Flask, request, render_template
+from flask import Flask, render_template, redirect, url_for, request
 
 app = Flask(__name__)
 
 @app.route('/')
-def homepage():
-    """A homepage with handy links for your convenience."""
-    return render_template('login.html')
+def login():
+    error = None
+    if request.method == 'POST':
+        if request.form['username'] != 'admin' or request.form['password'] != 'admin':
+            error = 'Invalid Credentials. Please try again.'
+        else:
+            return redirect(url_for('home'))
+    return render_template('login.html', error=error)
+
+
+
+@app.route('/search', methods=['GET', 'POST'])
+def search():
+    
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
