da6bf9a9c89ebcc738035c09dc1f6bd8f5f059c4
Jairo Sanchez <jairosanchez@Jairos-MacBook-Air.local>
2020-12-04 13:49:53 -0800

    added a very simple prototype for search page

--- test.py
+++ test.py
@@ -3,22 +3,23 @@ from flask import Flask, render_template, redirect, url_for, request
 
 app = Flask(__name__)
 
-@app.route('/')
+
+
+@app.route('/search', methods=['GET', 'POST'])
+def search():
+    return render_template('search.html')
+
+@app.route('/', methods=['GET', 'POST'])
 def login():
     error = None
     if request.method == 'POST':
         if request.form['username'] != 'admin' or request.form['password'] != 'admin':
             error = 'Invalid Credentials. Please try again.'
         else:
-            return redirect(url_for('home'))
+            return redirect(url_for('search'))
     return render_template('login.html', error=error)
 
-
-
-@app.route('/search', methods=['GET', 'POST'])
-def search():
-    
-
+@app.route('/article_results')
 
 if __name__ == '__main__':
     app.run(debug=True)
