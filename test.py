899382f8ec9ca28b039decf6830ef2f66081631a
Jairo Sanchez <jairosanchez@Jairos-MacBook-Air.local>
2020-12-04 17:44:01 -0800

    Took out some navbars and search route is functional

--- test.py
+++ test.py
@@ -4,22 +4,31 @@ from flask import Flask, render_template, redirect, url_for, request
 app = Flask(__name__)
 
 
+@app.route('/search_results', methods=['GET', 'POST'])
+def search_results():
+    return render_template('search_results.html')
 
 @app.route('/search', methods=['GET', 'POST'])
 def search():
-    return render_template('search.html')
+    error = None
+    if request.method == 'POST':
+        if request.form.get('search') != 'article':
+            error = 'Please input a keyword or search term'
+        else:
+            return redirect(url_for('search_results'))
+            
+    return render_template('search.html', error=error)
 
 @app.route('/', methods=['GET', 'POST'])
 def login():
     error = None
     if request.method == 'POST':
-        if request.form['username'] != 'admin' or request.form['password'] != 'admin':
+        if request.form['username'] != 'a' or request.form['password'] != 'a':
             error = 'Invalid Credentials. Please try again.'
         else:
             return redirect(url_for('search'))
-    return render_template('login.html', error=error)
 
-@app.route('/article_results')
+    return render_template('login.html', error=error)
 
 if __name__ == '__main__':
     app.run(debug=True)
